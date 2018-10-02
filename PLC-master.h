eaa6f87cd277264623f31818e10e1215135d12e4
hienpvptit <36995026+hienpvptit@users.noreply.github.com>
2018-10-02 14:05:05 +0700

    Add files via upload

---
+++ PLC-master.h
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <stdint.h>
+#include <string>
+
+
+class Master{
+	public:
+		Master();
+		void InsertDevice(uint8_t id_device);
+		int8_t FindDevice(uint8_t id_device);
+		void DeleteDevice(uint8_t id_device);
+	private:
+		uint8_t num_devices;
+		uint8_t id_devices[20];	
+};
+
+class Frame{
+	public:
+		Frame();
+		void Reset();
+		void Catch(uint8_t ch);
+		bool Get_checkStart();
+		bool Get_checkStop();
+	private:
+		bool check_start;
+		bool check_stop;
+		string buff;
+		string data_frame;	
+};
+
+
