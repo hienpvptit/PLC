496bae05735f44a3c044dc5702c0c8062720d503
hienpvptit <36995026+hienpvptit@users.noreply.github.com>
2018-11-13 16:07:19 +0700

    Add files via upload

--- Master/main.c
+++ Master/main.c
@@ -3,6 +3,12 @@
 #include "lib/PLC-uart.h"
 #include "lib/PLC-master.h"
 
+void test()
+{
+  unsigned int i;
+  for(i=0 ;i< 50000; i++){};
+}
+
 int main( void )
 {
   Master_Reset();
@@ -16,12 +22,14 @@ int main( void )
   
   while(1)
   {
-    if(Q_data.item_count != 0)
-    {
-      Frame_Catch(Queue_Pop());
-      Master_Update_2();
-    }
-    Master_Update_1();
+//    if(Q_data.item_count != 0)
+//    {
+//      Frame_Catch(Queue_Pop());
+//      Master_Update_2();
+//    }
+//    Master_Update_1();
+    Fn_UART_Puts("Delete Device! ID = ");
+    test();
   }
   
 }
